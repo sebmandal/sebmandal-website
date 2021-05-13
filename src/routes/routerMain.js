4972c25fd9323b2f4fe4a6cceab3f03cf2f8d7d4
Sebastian Mandal (sebmandal) <sebastian.mandal@icloud.com>
2021-05-13 20:29:42 +0000

    changed a few things here and there

--- src/routes/routerMain.js
+++ src/routes/routerMain.js
@@ -25,11 +25,16 @@ router.get('/history', function (req, res) {
 });
 
 router.get('/detail/:articleID', function (req, res) {
+  req.session.url = '/detail';
   var articles = fs.readJSONSync('./src/db/articles.json');
   req.session.articles = articles;
   req.session.article = req.session.articles[parseInt(req.params.articleID)];
   res.render('detailView', req.session);
 });
+
+// reidrection router
+var redirects = require('./routerRedirects')
+router.use(redirects);
 
 // to be able to require this from server.js
 module.exports = router;
